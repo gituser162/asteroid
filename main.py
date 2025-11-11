ad93cb519b13cd7022b28d351efc9fb56fe28636
gituser162 <keelahselai98@gmail.com>
2025-11-11 14:31:48 +0000

    player added

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE_SECONDS, ASTEROID_MAX_RADIUS
+from player import Player
 
 def main():
+    dt = 0
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -12,15 +19,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
-        
-    print("Starting Asteroids!")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Asteroid min radius: {ASTEROID_MIN_RADIUS}")
-    print(f"Asteroid kinds: {ASTEROID_KINDS}")
-    print(f"Asteroid spawn rate seconds: {ASTEROID_SPAWN_RATE_SECONDS}")
-    print(f"Asteroid max radius: {ASTEROID_MAX_RADIUS}")
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
+    
+    # print("Starting Asteroids!")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Asteroid min radius: {ASTEROID_MIN_RADIUS}")
+    # print(f"Asteroid kinds: {ASTEROID_KINDS}")
+    # print(f"Asteroid spawn rate seconds: {ASTEROID_SPAWN_RATE_SECONDS}")
+    # print(f"Asteroid max radius: {ASTEROID_MAX_RADIUS}")
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,41 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLATER_TURN_SPEED, PLAYER_SPEED
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(
+            screen,
+            "white",
+            self.triangle(),
+            LINE_WIDTH  # line width
+        )
+    def rotate(self,dt):
+        self.rotation += PLATER_TURN_SPEED * dt
+    # in the Player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+             self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+    
+    def move(self, dt):
+        self.position += (pygame.Vector2(0,1).rotate(self.rotation)) * (PLAYER_SPEED * dt)
